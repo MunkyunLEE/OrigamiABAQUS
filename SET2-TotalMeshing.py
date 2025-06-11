4753cf5f75f0691efde2b0789f57a7f583849f7a
Munkyun Lee <89787280+MunkyunLEE@users.noreply.github.com>
2025-06-11 15:25:07 +0900

    Update SET2-TotalMeshing.py
    Edge division calculation based on the shortest edge length for Meshing Shell/Solid elements
    !! NOTE that this code cannot ensure the meshing, It just give you the edge division

--- SET2-TotalMeshing.py
+++ SET2-TotalMeshing.py
@@ -24,12 +24,12 @@ for part_name in model.parts.keys():
         
 min_edge_length = min(all_edge_lengths)
 Edge_scaling_factor = 50000 # Edge scale control (Manual, avg, var, std)
-# print("Average:", numpy.mean(all_edge_lengths))
-# print("Variance:", numpy.var(all_edge_lengths))
-# print("Standard:", numpy.std(all_edge_lengths))
 Mesh_scaling_factor = 50
-n = 10  # 최소 엣지에 대한 분할 수 (Even Number Recommended)
 
+# # # # # # # # # # # # # # # # # # # 
+# 최소 엣지에 대한 기준 분할 수 (Even Number Recommended)
+n = 10
+# # # # # # # # # # # # # # # # # # # 
 
 for part_name in model.parts.keys():
     part = model.parts[part_name]
@@ -48,38 +48,40 @@ for part_name in model.parts.keys():
         if edge_length / min_edge_length > Edge_scaling_factor:
             # num_divisions = math.ceil(math.log10(num_divisions) * Mesh_scaling_factor)
             num_divisions = math.ceil((edge_length / min_edge_length) * n / 4)  # 소수점 올림
-
-        # Bi-Linear Mesh Scaling
-        # if edge_length / min_edge_length > Edge_scaling_factor:
-        #     num_divisions = math.ceil(num_divisions / Mesh_scaling_factor)
         
         if num_divisions % 2 != 0:  # 홀수일 경우
             num_divisions += 1  # 1을 더해 짝수로 만듦
+
+        # 그냥 전체 엣지에 대해 분할 수를 지정해 버리고 싶다면
+        # num_divisions = 50
         
         part.seedEdgeByNumber(edges=(edge,), number=num_divisions, constraint=FIXED)
         # print(f"Part {part_name}, Edge {edge.index}: Divided into {num_divisions} sections.")
     
     
-    if part.cells:  # 셀이 존재하는 경우 Solid Element Meshing
+    if part.cells:  # Solid Element Meshing
         part.setMeshControls(regions=part.cells, elemShape=HEX, technique=SWEEP, algorithm=MEDIAL_AXIS)
         elem_type = mesh.ElemType(elemCode=C3D20R, elemLibrary=STANDARD)  # TET 2차 요소
         part.setElementType(regions=(part.cells,), elemTypes=(elem_type,))
-    else:  # Face 존재하는 경우 Shell Element Meshing
+    else:  # Shell Element Meshing
         faces = part.faces
         part.setMeshControls(regions=faces, elemShape=QUAD, technique=FREE, algorithm=MEDIAL_AXIS)
         elem_type = mesh.ElemType(elemCode=S8R, elemLibrary=STANDARD)  # QUAD 2차 요소
         part.setElementType(regions=(faces,), elemTypes=(elem_type,))
 
-# SHELL
+# # # # # # # # # # # # # # # # # # # 
+# # # # # 선택 옵션들
+# # # # # SHELL
 # elemShape= QUAD, QUAD_DOMINATED, TRI
-# SOLID
+# elemCode = S4R, S8R
+# # # # # SOLID
 # elemShape= HEX, HEX_DOMINATED, TET, WEDGE
 # technique= FREE, STRUCTURED, SWEEP
 # algorithm= ADVANCING_FRONT, MEDIAL_AXIS
 # Meshing Quadratic
 # Shell: Quad >> S8R, TRI >>STRI65
 # Solid* HEX >> C3D20R
-
+# # # # # # # # # # # # # # # # # # # 
 
 # 메쉬 생성
 for part_name in model.parts.keys():
